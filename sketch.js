9c6a3899130ca3f540103040142429eadc2c4857
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-01-31 05:07:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,24 +18,10 @@ function setup() {
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
 
-   for (var k = 0; k <=width; k = k + 80) {
-     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-   }
-    for (var j = 75; j <=width; j=j+50) {
-       plinkos.push(new Plinko(j,75));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) {
-        plinkos.push(new Plinko(j,175));
-    }
-
-    for (var j = 75; j <=width; j=j+50) {
-        plinkos.push(new Plinko(j,275));
-    }
-
-    for (var j = 50; j <=width-10; j=j+50) {
-        plinkos.push(new Plinko(j,375));
-    }
+  //add for loops to increment scores
+    
+    
+    
     
 }
  
@@ -119,7 +105,6 @@ function mousePressed()
 {
   if(gameState!=="end")
   {
-      count++;
-     particle=new Particle(mouseX, 10, 10, 10); 
+      //increment count and add text mouseX and Y
   }   
 }
